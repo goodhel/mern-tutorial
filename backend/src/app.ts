1d80b2d8808cf799c777a3b716da6ea149b17f65
yudhie123 <yudhie123@gmail.com>
2022-02-19 14:16:15 +0700

    feat: serve frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { routes } from "./routes"
 import connectDB from "./helpers/database"
+import path from 'path'
 
 connectDB()
 
@@ -15,11 +16,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get('/', async (req: Request, res: Response, _next: NextFunction) => {
-    res.status(200).send({ data: 'Hello this is API From Mern Tutorial' })
-})
+// app.get('/', async (req: Request, res: Response, _next: NextFunction) => {
+//     res.status(200).send({ data: 'Hello this is API From Mern Tutorial' })
+// })
 
 routes(app)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../frontend/build')))
+
+    app.get('*', (req: Request, res: Response) => 
+        res.sendFile(
+            path.resolve(__dirname, '../', '../', 'frontend', 'build', 'index.html')
+        )
+    )
+} else {
+    app.get('/', (req: Request, res: Response) => res.send('Please set to production'))
+}
 
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
